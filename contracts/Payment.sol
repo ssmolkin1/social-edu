22da2b5d42ffd762282504d7e03d57f29246704d
Sam Smolkin <sam@future-precedent.org>
2019-07-06 23:29:24 +0100

    progress on bug fixing in Payment.sol

--- contracts/Payment.sol
+++ contracts/Payment.sol
@@ -8,45 +8,47 @@ contract Payment is Ownable {
     using SafeMath for uint256;
 
     event Deposited(address indexed _depositor, uint256 _weiAmount);
-    event Payed(address _recipient, address _amount);
+    event Payed(address[] _recipients, address[] _payments);
 
     Escrow private escrow = new Escrow();
 
     function getReward() public view returns (uint256) {
-        return escrow.depositOf(address(this));
+        return escrow.depositsOf(address(this));
     }
 
     function deposit() public payable {
         escrow.deposit(address(this));
 
         emit Deposited(msg.sender, msg.value);
-    }       
+    }
 
     function withdraw() public onlyOwner {
         escrow.withdraw(address(this));
     }
 
-    function Pay(address[] _recipients, address[] _payments) public onlyOwner {
+    function Pay(address[] memory _recipients, address[] memory _payments) public onlyOwner validPayment(_recipients, _payments) {
+        uint256 rl = _recipients.length;
 
         for (uint256 i = 0; i < rl; i++) {
-            uint256 payment = _payments[i] < address(this).balance ? _payments[i] : address(this).balance ;
-            
-            _
+            _recipients[i].transfer(_payments[i]);
         }
+
+        emit Payed(_recipients, _payments);
     }
 
-    modifier validPayment(address[] _recipients, address[] _payments) {
-        uint256 rl = _recipeints.length;
+    modifier validPayment(address[] memory _recipients, address[] memory _payments) {
+        uint256 rl = _recipients.length;
         require (rl > 0, "No recipients.");
 
-        uint256 pl = _payments.length; 
+        uint256 pl = _payments.length;
         require (rl == pl, "Number of recepients and payments do not match.");
 
         require(sum(_payments) == address(this).balance, "Payments not equal to balance");
+
         _;
     }
 
-    function sum(uint256 _array) internal return (uint256) {
+    function sum(uint256[] memory _array) internal returns (uint256) {
         uint256 l = _array.length;
         uint256 result = 0;
 
