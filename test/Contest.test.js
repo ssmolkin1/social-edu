ca290c13626916b60dd4e6d02a67766c35de92cf
Sam Smolkin <sam@future-precedent.org>
2019-07-07 01:31:14 +0100

    got reward test to work

--- test/Contest.test.js
+++ test/Contest.test.js
@@ -29,22 +29,40 @@ contract("Contest", async accounts => {
   });
 
   it("Should be able to withdraw funds from escrow", async () => {
-    const reward = await contest.getReward();
-    assert.strictEqual(reward.toNumber(), 0);
+    const deposits = 600;
 
-    const d1 = 100;
-    const d2 = 200;
-    const d3 = 300;
-    const deposits = d1 + d2 + d3;
-
-    await contest.deposit({ from: accounts[1], value: d1 });
-    await contest.deposit({ from: accounts[2], value: d2 });
-    await contest.deposit({ from: accounts[3], value: d3 });
+    await contest.deposit({ from: accounts[0], value: deposits });
 
     await contest.withdraw();
 
-    balance = await web3.eth.getBalance(contest.address);
+    const balance = await web3.eth.getBalance(contest.address);
     assert.strictEqual(parseInt(balance), deposits);
+  });
+
+  it("Should be able to distribute rewards", async () => {
+    const deposits = 2;
+    const depositsInWei = web3.utils.toWei(deposits.toString(), "ether");
+
+    await contest.deposit({ from: accounts[0], value: depositsInWei });
+
+    await contest.withdraw();
+
+    const balance = await web3.eth.getBalance(contest.address);
+    assert.strictEqual(balance, depositsInWei);
+
+    const bBefore1 = web3.utils.fromWei(await web3.eth.getBalance(accounts[1]));
+    const bBefore2 = web3.utils.fromWei(await web3.eth.getBalance(accounts[2]));
+
+    await contest.DistributeRewards([accounts[1], accounts[2]], [50, 50]);
+
+    const bAfter1 = web3.utils.fromWei(await web3.eth.getBalance(accounts[1]));
+    const bAfter2 = web3.utils.fromWei(await web3.eth.getBalance(accounts[2]));
+
+    const diff1 = parseInt(bAfter1) - parseInt(bBefore1);
+    const diff2 = parseInt(bAfter2) - parseInt(bBefore2);
+
+    assert.strictEqual(diff1, deposits / 2);
+    assert.strictEqual(diff2, deposits / 2);
   });
 
   /*   it("Should be named Content Bounty Coin", async () => {
